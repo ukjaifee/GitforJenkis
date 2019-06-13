3b11449f29522507c177164d2c4e9afcb368c72a
ukjaifee@gmail.com <ukjaifee@gmail.com>
2019-06-13 18:42:16 +0530

    My first code

---
+++ src/main/java/jenkins/newjob/myTest.java
@@ -0,0 +1,14 @@
+package jenkins.newjob;
+
+import org.testng.annotations.Test;
+
+public class myTest {
+	
+	
+	@Test
+	public void jenkisTest() {
+		
+		System.out.println("I  am testing");
+	}
+
+}
